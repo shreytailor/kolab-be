001455869995f6911c5997e9378fdf6d951e0df1
shreytailor <shreym.tailor@gmail.com>
2020-09-13 01:01:40 +1200

    testing. :rocket:

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
-const port = process.env.PORT || 3015;
+const port = process.env.PORT || 3001;
 
 // Setting up our application.
 const kolab = express();
@@ -10,23 +10,7 @@ kolab.use(bodyparser.urlencoded({extended: true}));
 kolab.use(bodyparser.urlencoded({ extended: true }));
 kolab.use(bodyparser.json());
 kolab.use(bodyparser.raw());
-
-let allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "http://kolab17.azurewebsites.net", "https://kolab17.azurewebsites.net"];
-kolab.use(cors({
-    origin: function(origin, callback) {
-                if(!origin) {
-                    return callback(null, true);
-                }
-                
-                if (allowedOrigins.indexOf(origin) === -1) {
-                let msg = 'The CORS policy for this site does not ' +
-                            'allow access from the specified Origin.';
-                return callback(new Error(msg), false);
-                }
-                
-                return callback(null, true);
-            }
-}));
+kolab.use(cors());
 
 // Importing all the required endpoints.
 const welcome = require('./api/welcome');
