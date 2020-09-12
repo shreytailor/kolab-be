c0473222368f211682ec3c179c5d3a4bbae07ded
shreytailor <shreym.tailor@gmail.com>
2020-09-12 18:53:58 +1200

    cors issue. :bug:

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
     <img alt="build:passing" src="https://img.shields.io/badge/Build-Passing-brightgreen">
     <img alt="licence:mit" src="https://img.shields.io/badge/Licence-MIT-9cf">
 </div>
+
+The origin for all the endpoints for this API is `http://kolab-be.azurewebsites.net/api/`.
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Importing the important packages.
 const express = require('express');
+const cors = require('cors');
 const bodyparser = require('body-parser');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3015;
 
 // Setting up our application.
 const kolab = express();
@@ -9,6 +10,23 @@ kolab.use(bodyparser.urlencoded({extended: true}));
 kolab.use(bodyparser.urlencoded({ extended: true }));
 kolab.use(bodyparser.json());
 kolab.use(bodyparser.raw());
+
+let allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "http://kolab17.azurewebsites.net", "https://kolab17.azurewebsites.net"];
+kolab.use(cors({
+    origin: function(origin, callback) {
+                if(!origin) {
+                    return callback(null, true);
+                }
+                
+                if (allowedOrigins.indexOf(origin) === -1) {
+                let msg = 'The CORS policy for this site does not ' +
+                            'allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+                }
+                
+                return callback(null, true);
+            }
+}));
 
 // Importing all the required endpoints.
 const welcome = require('./api/welcome');
