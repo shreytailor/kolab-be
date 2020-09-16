2f805c198a3136de1444d98ecd6790a354312ff2
shreytailor <shreym.tailor@gmail.com>
2020-09-17 11:34:40 +1200

    testing getAll endpoint. :ambulance:

--- api/question/getAll.js
+++ api/question/getAll.js
@@ -5,7 +5,9 @@ const connection = require('./../../db/dbConnect');
 router.get("/", function (request, response, next) {
     // Performing the SQL query to simply get all the data.
     connection.query(`select * from Questions order by time_created desc`, function (error, results, fields) {
-        if (error) throw error;
+        if (error) {
+            response.status(404).send(error);
+        }
         response.status(200).send(results);
     })
 })
