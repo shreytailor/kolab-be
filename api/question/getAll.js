c16515c06679228afe950c2dca05210bf47d2392
shreytailor <shreym.tailor@gmail.com>
2020-09-17 13:41:55 +1200

    temporary commit.

--- api/question/getAll.js
+++ api/question/getAll.js
@@ -5,7 +5,11 @@ const connection = require('./../../db/dbConnect');
 router.get("/", function (request, response, next) {
     // Performing the SQL query to simply get all the data.
     connection.query(`select * from Questions order by time_created desc`, function (error, results, fields) {
-        response.send(error + "___" + results + "___" + fields);
+        if (error) {
+            response.status(404).send(error);
+        };
+        
+        response.status(200).send(results);
     })
 })
 
